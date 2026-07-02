8c86041d108eb38a5b64e59a17edc12ac796b3e1
salehabdulla <salehabdullaict@gmail.com>
2026-07-02 19:26:33 +0300

    refactor: update NewRepo function to include AuthService parameter

--- pkg/handlers/home.go
+++ pkg/handlers/home.go
@@ -6,17 +6,20 @@ import (
 	"real-time-forum/pkg/config"
 	"real-time-forum/pkg/models"
 	"real-time-forum/pkg/render"
+	"real-time-forum/pkg/service"
 )
 
 var Repo *Repository
 
 type Repository struct {
-	App *config.AppConfig
+	App         *config.AppConfig
+	AuthService service.AuthService
 }
 
-func NewRepo(a *config.AppConfig) *Repository {
+func NewRepo(a *config.AppConfig, as service.AuthService) *Repository {
 	return &Repository{
-		App: a,
+		App:         a,
+		AuthService: as,
 	}
 }
 
@@ -26,10 +29,9 @@ func NewHandlers(r *Repository) {
 
 // Home is the home page handler
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
-	
 	remoteIP := r.RemoteAddr
 	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
-	
+
 	if err := render.RenderTemplate(w, &models.TemplateData{}); err != nil {
 		log.Fatal(err.Error())
 	}
