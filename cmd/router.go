d3e2346da40f0525c444c3996f0255b6e84cffdd
salehabdulla <salehabdullaict@gmail.com>
2026-07-02 19:25:58 +0300

    register login route

--- cmd/router.go
+++ cmd/router.go
@@ -12,8 +12,9 @@ func routes() http.Handler {
 	mux := chi.NewRouter()
 	mux.Use(middleware.Recoverer)
 	mux.Use(SessionLoad)
-	
+
 	mux.Get("/", handlers.Repo.Home)
-	mux.Post("/api/v1/auth/register", handlers.Repo.Register);
+	mux.Post("/api/v1/auth/register", handlers.Repo.Register)
+	mux.Post("/api/v1/auth/login", handlers.Repo.Login)
 	return mux
 }
