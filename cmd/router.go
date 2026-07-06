0ecacf7ab9f954ed3ab81760686dcaeb7333610b
salehabdulla <salehabdullaict@gmail.com>
2026-07-06 17:45:06 +0300

    style: format code for improved readability in router.go

--- cmd/router.go
+++ cmd/router.go
@@ -10,25 +10,27 @@ import (
 )
 
 func routes() http.Handler {
-    mux := chi.NewRouter()
-    mux.Use(chiMiddleware.Recoverer)
-    mux.Use(RequestLogger)
-    mux.Use(SessionLoad)
+	mux := chi.NewRouter()
+	mux.Use(chiMiddleware.Recoverer)
+	mux.Use(RequestLogger)
+	mux.Use(SessionLoad)
 
-    mux.Group(func(r chi.Router) {
-        r.Post("/api/v1/auth/register", handlers.Repo.Register)
-        r.Post("/api/v1/auth/login", handlers.Repo.Login)
-    })
+	mux.Group(func(r chi.Router) {
+		r.Post("/api/v1/auth/register", handlers.Repo.Register)
+		r.Post("/api/v1/auth/login", handlers.Repo.Login)
+		
+	})
 
-    mux.Group(func(r chi.Router) {
-        r.Use(pkgmiddleware.AuthMiddleware)
+	mux.Group(func(r chi.Router) {
+		r.Use(pkgmiddleware.AuthMiddleware)
 
-        r.Get("/", handlers.Repo.Home)
-        
+		r.Get("/", handlers.Repo.Home)
+
         r.Post("/api/v1/auth/logout", handlers.Repo.Logout)
-        r.Get("/api/v1/auth/me", handlers.Repo.Me)
+		r.Get("/api/v1/auth/me", handlers.Repo.Me)
+		r.Get("/api/v1/categories", handlers.Repo.GetCategories)
         
-    })
+	})
 
-    return mux
+	return mux
 }
