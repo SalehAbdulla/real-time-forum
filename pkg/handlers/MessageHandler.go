3438921e391bcc9d8905bcee079619a9649dbc15
salehabdulla <salehabdullaict@gmail.com>
2026-07-08 23:17:36 +0300

    feat: implement GetChatUsers handler to retrieve chat users

---
+++ pkg/handlers/MessageHandler.go
@@ -0,0 +1,29 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	realtimeforum "real-time-forum"
+	"real-time-forum/pkg/middleware"
+	"real-time-forum/pkg/models"
+	"real-time-forum/pkg/payload/message"
+)
+
+func (re *Repository) GetChatUsers(w http.ResponseWriter, r *http.Request) {
+	userID, ok := middleware.UserIDFromContext(r.Context())
+	if !ok || userID == "" {
+		re.HandleError(w, r, realtimeforum.ErrUnauthorized)
+		return
+	}
+
+	users, err := re.MessageService.GetChatUsers(userID)
+	if err != nil {
+		re.HandleError(w, r, err)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(models.DataResponse[[]message.ChatUserDTO]{
+		Data: users,
+	})
+}
