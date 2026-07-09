baebe371e9b2c360a7c297082f28e572e74f6056
salehabdulla <salehabdullaict@gmail.com>
2026-07-09 03:10:09 +0300

    feat: add MessagesResponse struct for message response representation

--- pkg/payload/message/MessagesResponse.go
+++ pkg/payload/message/MessagesResponse.go
@@ -1,1 +1,8 @@
 package message
+
+type MessagesResponse struct {
+	Messages      []MessageDTO `json:"messages"`
+	Offset        int          `json:"offset"`
+	Limit         int          `json:"limit"`
+	TotalElements int          `json:"totalElements"`
+}
