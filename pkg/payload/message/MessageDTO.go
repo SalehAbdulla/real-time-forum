751c1dee113b6e7716f936a0ece541a15dc88832
salehabdulla <salehabdullaict@gmail.com>
2026-07-09 03:10:03 +0300

    feat: define MessageDTO struct for message payload representation

--- pkg/payload/message/MessageDTO.go
+++ pkg/payload/message/MessageDTO.go
@@ -1,1 +1,10 @@
 package message
+
+type MessageDTO struct {
+	MessageId   int    `json:"messageId"`
+	SenderId    string `json:"senderId"`
+	RecipientId string `json:"recipientId"`
+	TextMessage string `json:"textMessage"`
+	TimeStamp   string `json:"timeStamp"`
+	IsRead      int    `json:"isRead"`
+}
