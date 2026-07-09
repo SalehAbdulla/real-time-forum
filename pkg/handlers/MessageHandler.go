30469738ac28aeb8af99f67a184e4668a86c1c83
salehabdulla <salehabdullaict@gmail.com>
2026-07-09 03:09:52 +0300

    feat: implement GetChatMessages handler for retrieving chat messages

--- pkg/handlers/MessageHandler.go
+++ pkg/handlers/MessageHandler.go
@@ -7,7 +7,46 @@ import (
 	"real-time-forum/pkg/middleware"
 	"real-time-forum/pkg/models"
 	"real-time-forum/pkg/payload/message"
+	"strconv"
 )
+
+func (re *Repository) GetChatMessages(w http.ResponseWriter, r *http.Request) {
+	userID, ok := middleware.UserIDFromContext(r.Context())
+	if !ok || userID == "" {
+		re.HandleError(w, r, realtimeforum.ErrUnauthorized)
+		return
+	}
+
+	conversationPartnerID := r.URL.Query().Get("partnetId")
+	if conversationPartnerID == "" {
+		re.HandleError(w, r, realtimeforum.ErrBadRequest)
+		return
+	}
+
+	offsetStr := r.URL.Query().Get("offset")
+	if offsetStr == "" {
+		offsetStr = "0"
+	}
+
+	offset, err := strconv.Atoi(offsetStr)
+	if err != nil || offset < 0 {
+		re.HandleError(w, r, realtimeforum.ErrBadRequest)
+		return
+	}
+
+	limit := 10
+
+	response, err := re.MessageService.GetMessages(conversationPartnerID, userID, offset, limit)
+	if err != nil {
+		re.HandleError(w, r, err)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(models.DataResponse[message.MessagesResponse]{
+		Data: response,
+	})
+}
 
 func (re *Repository) GetChatUsers(w http.ResponseWriter, r *http.Request) {
 	userID, ok := middleware.UserIDFromContext(r.Context())
