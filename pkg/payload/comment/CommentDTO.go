af829dd4b680357c7b1dc64c6bccb6f0fe9ee3a4
salehabdulla <salehabdullaict@gmail.com>
2026-07-18 19:51:11 +0300

    feat: add Nickname and Score fields to CommentDTO for enhanced comment details

--- pkg/payload/comment/CommentDTO.go
+++ pkg/payload/comment/CommentDTO.go
@@ -4,6 +4,8 @@ type CommentDTO struct {
 	CommentId   int    `json:"commentId"`
 	PostId      int    `json:"postId"`
 	UserId      string `json:"userId"`
+	Nickname    string `json:"nickname"`
 	CommentText string `json:"commentText"`
+	Score       int    `json:"score"`
 	CreatedAt   string `json:"createdAt"`
 }
